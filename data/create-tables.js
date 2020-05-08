7ef8339d7cab5c7db60c3458cab8fd4b18f78c04
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-07 17:00:06 -0700

    file setup and deployed to pg admin

---
+++ data/create-tables.js
@@ -0,0 +1,40 @@
+const client = require('../lib/client');
+
+// async/await needs to run in a function
+run();
+
+async function run() {
+
+  try {
+    // initiate connecting to db
+    await client.connect();
+
+    // run a query to create tables
+    await client.query(`
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY,
+                    email VARCHAR(256) NOT NULL,
+                    hash VARCHAR(512) NOT NULL
+                );           
+                CREATE TABLE minerals (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    name VARCHAR(512) NOT NULL,
+                    vibrates_to INTEGER NOT NULL,
+                    healing BOOLEAN NOT NULL,
+                    associated_signs VARCHAR(512) NOT NULL,
+                    chakra VARCHAR(512) NOT NULL
+            );
+        `);
+
+    console.log('create tables complete');
+  }
+  catch(err) {
+    // problem? let's see the error...
+    console.log(err);
+  }
+  finally {
+    // success or failure, need to close the db connection
+    client.end();
+  }
+
+}
---
+++ data/minerals.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    name: 'Amethyst',
+    vibrates_to: 3,
+    healing: true,
+    associated_signs: 'pisces',
+    chakra: 'crown'
+  },
+  {
+    name: 'Opal',
+    vibrates_to: 8,
+    healing: true,
+    associated_signs: 'libra',
+    chakra: 'crown'
+  }
+];
