19fa9fc021cbd71aff5fc9a8f4e93a74d6eaac41
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-13 13:17:08 -0700

    adds delete route, tested and verified in postman, cleans up server file

--- server.js
+++ server.js
@@ -13,34 +13,34 @@ app.get('/minerals', async(req, res) => {
   //triple check on colors.color in SELECT statement and if it should be plural or singular (re: line 41 in load-seed-data.js)
   const data = await client.query(`
   SELECT minerals.id, minerals.name, minerals.vibrates_to, minerals.rarity, minerals.associated_signs, minerals.chakra, colors.color
-  from minerals
-  join colors
-  on minerals.color_id = colors.id`);
+  FROM minerals
+  JOIN colors
+  ON minerals.color_id = colors.id`);
 
-  res.json(data.rows);
+  res.json(data.rows[0]);
 });
 
 //gets all colors
 app.get('/colors', async(req, res) => {
-  const data = await client.query('SELECT * from colors');
+  const data = await client.query('SELECT * FROM colors');
 
-  res.json(data.rows);
+  res.json(data.rows[0]);
 });
 
 //gets all users
 app.get('/users', async(req, res) => {
-  const data = await client.query('SELECT * from users');
+  const data = await client.query('SELECT * FROM users');
 
-  res.json(data.rows);
+  res.json(data.rows[0]);
 });
 
 //gets one color
 app.get('/color/:id', async(req, res) => {
   const id = req.params.id;
-  const data = await client.query('SELECT * from colors where id=$1',
+  const data = await client.query('SELECT * FROM colors WHERE id=$1',
     [id]
   );
-  res.json(data.rows);
+  res.json(data.rows[0]);
 });
 
 //gets one mineral
@@ -48,10 +48,10 @@ app.get('/mineral/:id', async(req, res) => {
 
   const data = await client.query(`
   SELECT minerals.id, minerals.name, minerals.vibrates_to, minerals.rarity, minerals.associated_signs, minerals.chakra, colors.color
-  from minerals
-  join colors
-  on minerals.color_id = colors.id
-  where minerals.id=$1`,
+  FROM minerals
+  JOIN colors
+  ON minerals.color_id = colors.id
+  WHERE minerals.id=$1`,
   [req.params.id]
   );
   res.json(data.rows[0]);
@@ -59,28 +59,40 @@ app.get('/mineral/:id', async(req, res) => {
 
 //creates a mineral
 app.post('/minerals', async(req, res) => {
-  console.log(req.body);
+
   const data = await client.query(
-    `insert into minerals (name, vibrates_to, rarity, associated_signs, chakra, color_id, user_id)
-    values ($1, $2, $3, $4, $5, $6, $7)
-    returning *;`,
+    `INSERT INTO minerals (name, vibrates_to, rarity, associated_signs, chakra, color_id, user_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *;`,
     [req.body.name, req.body.vibrates_to, req.body.rarity, req.body.associated_signs, req.body.chakra, req.body.color_id, req.body.user_id]
   );
 
-  res.json(data.rows);
+  res.json(data.rows[0]);
 });
 
 //allows user to update a resource and returns full copy of resource from database
 app.put('/minerals/:id', async(req, res) => {
+
   const data = await client.query(`
     UPDATE minerals
     SET associated_signs = $2,
         chakra = $3
     WHERE id=$1 
-    returning *;
+    RETURNING *;
   `, [req.params.id, req.body.associated_signs, req.body.chakra]);
 
-  res.json(data.rows);
+  res.json(data.rows[0]);
+});
+
+app.delete('/minerals/:id', async(req, res) => {
+
+  const data = await client.query(`
+  DELETE FROM minerals
+  WHERE id=$1
+  RETURNING *;
+  `, [req.params.id]);
+
+  res.json(data.rows[0]);
 });
 
 app.listen(PORT, () => {
